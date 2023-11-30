b02baa8230a8abeffb153d2faeab70d69654a9f2
tamar5483 <tamar5483@gmail.com>
2023-11-30 13:55:31 +0200

    hebrew documention

--- practicode4/Program.cs
+++ practicode4/Program.cs
@@ -2,25 +2,36 @@
 using System.CommandLine;
 using System.Reflection;
 
+//פקודת השורש
 var rootCommand = new RootCommand("This is a root command");
 
+//שתי פקודות-הראשונה אוספת את כל הקבצים לקובץ אחד לפי האופתציות ששלוחים לה, והשנייה קולטת מהמשתמש את האופציות ויוצרת קובץ עם הפקודה הראשונה והאופציות שקלטה משמשתמש
 var bundle = new Command("bundle", "bundle files to one file");
 var createRsp = new Command("create-rsp", "create rsp file");
 
+//אופציות, לכל אופציה יש גם אליאס - קיצור
+//שם הקובץ שיווצר ואליו יועתקו שאר הקבצים
 var outputOption = new Option<FileInfo>("--output", "specify the output bundle file name");
 outputOption.AddAlias("-o");
+//האם לכתוב מעל כל חלק בקובץ החדש מאיזה קובץ הוא הועתק
 var noteOption = new Option<bool>("--note", "if write code source");
 noteOption.AddAlias("-n");
+//האם למחוק שורות ריקות 
 var removeEmptyLinesOption = new Option<bool>("--remove-empty-lines", "if write code source");
 removeEmptyLinesOption.AddAlias("-r");
+//איך למיין-לפי שם או שפה
 var sortOption = new Option<string>("--sort", () => "name", "sort name or language");
 sortOption.AddAlias("-s");
+//שם יוצר הקובץ שיהיה כתוב למעלה
 var authorOption = new Option<string>("--author", "add author name");
 authorOption.AddAlias("-a");
+//רשימת שפות שהקבצים שלהם יועתקו-חובה
+//שרושור של סיומות קבצים עם פסיקים בינהם
 var languageOption = new Option<string>("--language", "specify programming languages (all for all languages)");
 languageOption.IsRequired = true;
 languageOption.AddAlias("-l");
 
+//הוספת כל האופציות לפקודה
 bundle.AddOption(languageOption);
 bundle.AddOption(outputOption);
 bundle.AddOption(noteOption);
@@ -28,8 +39,10 @@ bundle.AddOption(sortOption);
 bundle.AddOption(authorOption);
 bundle.AddOption(removeEmptyLinesOption);
 
+//הפונקציה שמתבצעת בפקודה השנייה
 createRsp.SetHandler(() =>
 {
+    //קולטת מהשתמש ערך לכל האופציות של הפקודה הראשונה ויוצרת קובץ עם הפקודה
     using (StreamWriter sw = new StreamWriter("rsp-file.rst"))
     {
         sw.WriteLine("bundle ");
@@ -44,22 +57,25 @@ createRsp.SetHandler(() =>
     }
 });
 
+//הפונקציה שמתבצעת בפקודה הראשונה
 bundle.SetHandler((output, language, note, sort, removeEmptyLines, author) =>
 {
-
+    //המרת השפות לרשימה
     var languages = language.Split(",").ToList();
+    //אם המשתמש רוצה את כל השפות, אוסף את כל הקבצים בתקייה
     if (languages.Contains("all"))
     {
         languages = Directory.GetFiles(Environment.CurrentDirectory, "*.*", SearchOption.AllDirectories)
             .Select(file => Path.GetExtension(file).Remove(0, 1))
             .ToList();
     }
-
+    //אוסף את כל הקבצים של השפות שהמשתמש ביקש
     var filesToBundle = Directory.GetFiles(Environment.CurrentDirectory, "*.*", SearchOption.AllDirectories)
            .Where(file =>
       languages.Contains(Path.GetExtension(file).Remove(0, 1)))
       .ToList();
 
+    //ממיין
     if (sort == "name")
     {
         filesToBundle.Sort((a, b) => a.CompareTo(b));
@@ -70,10 +86,12 @@ bundle.SetHandler((output, language, note, sort, removeEmptyLines, author) =>
         filesToBundle.Sort((a, b) => Path.GetExtension(a).CompareTo(Path.GetExtension(b)));
     }
 
+    //העתקה לקובץ אחד
     try
     {
         using (StreamWriter sw = new StreamWriter(output.FullName))
         {
+            //אם המשתמש הכניס שם יוצר- כותב אותו בראש הקובץ
             if (author != null&&author!="")
                 sw.WriteLine("//author: " + author);
 
@@ -81,6 +99,8 @@ bundle.SetHandler((output, language, note, sort, removeEmptyLines, author) =>
             {
                 sw.WriteLine();
                 sw.WriteLine();
+                //אם המשתמש הכניס note כותב את קובץ המקור לפני כל קטע
+                if (note)
                 sw.WriteLine("//" + filePath);
                 sw.WriteLine();
                 using (StreamReader sr = new StreamReader(filePath))
@@ -88,6 +108,7 @@ bundle.SetHandler((output, language, note, sort, removeEmptyLines, author) =>
                     string line;
                     while ((line = sr.ReadLine()) != null)
                     {
+                        //אן השורה לא ריקה, או שהמשתמש לא ביקש למחוק שורות ריקות
                         if (line.Trim().Length > 0 || !removeEmptyLines) // Check if the line is not empty
                         {
                             sw.WriteLine(line);
